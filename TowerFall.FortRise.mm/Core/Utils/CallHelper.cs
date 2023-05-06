ed5e55a8f1ea5042f3984c25ae61dfbbf0ff966a
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-05-06 13:36:20 +0800

    Added a helper function to call a base function

---
+++ TowerFall.FortRise.mm/Core/Utils/CallHelper.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace FortRise;
+
+public static class CallHelper 
+{
+    public static Action<TTarget> CallBaseGen<TBase, TTarget>(string methodName) 
+    {
+        var targetType = typeof(TTarget);
+        var baseUpdateMethod = typeof(TBase).GetMethod(methodName);
+        var dm = new DynamicMethod("<Base>" + methodName, null, new Type[] { targetType }, targetType);
+        var gen = dm.GetILGenerator();
+        gen.Emit(OpCodes.Ldarg_0);
+        gen.Emit(OpCodes.Call, baseUpdateMethod);
+        gen.Emit(OpCodes.Ret);
+        return (Action<TTarget>)dm.CreateDelegate(typeof(Action<TTarget>));
+    }
+
+    public static Action<TTarget> CallBaseGen<TBase, TTarget>(string methodName, BindingFlags flags) 
+    {
+        var targetType = typeof(TTarget);
+        var baseUpdateMethod = typeof(TBase).GetMethod(methodName, flags);
+        var dm = new DynamicMethod("<Base>" + methodName, null, new Type[] { targetType }, targetType);
+        var gen = dm.GetILGenerator();
+        gen.Emit(OpCodes.Ldarg_0);
+        gen.Emit(OpCodes.Call, baseUpdateMethod);
+        gen.Emit(OpCodes.Ret);
+        return (Action<TTarget>)dm.CreateDelegate(typeof(Action<TTarget>));
+    }
+}
