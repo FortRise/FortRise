c2a4d52169d5b5064d9a244220eb47de6b65b24a
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-05-06 13:16:03 +0800

    Reorganize the file structure

--- TowerFall.FortRise.mm/old-build.bat
+++ TowerFall.FortRise.mm/old-build.bat
@@ -1,8 +1,8 @@
 dotnet build -c Release
 cd bin/Release/net472
-copy TowerFall.FortRise.mm.dll "../../../../../TowerFall.FortRise.mm.dll"
-copy TeuJson.dll "../../../../../TeuJson.dll"
-cd ../../../../../orig
+copy TowerFall.FortRise.mm.dll "../../../../../../TowerFall.FortRise.mm.dll"
+copy TeuJson.dll "../../../../../../TeuJson.dll"
+cd ../../../../../../orig
 copy "TowerFall.exe" "../TowerFall.exe"
 cd ../MonoMod
 MonoMod.exe ../TowerFall.exe
---
+++ lib-stripped/README.md
@@ -0,0 +1,4 @@
+This stripped libs inside here are stripped from BepInEx.AssemblyPublicizer
+https://github.com/BepInEx/BepInEx.AssemblyPublicizer
+
+# DO NOT PUSH THE ORIGINAL VERSION OF STRIPPED LIBS
