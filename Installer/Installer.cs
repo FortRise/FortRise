e79bc9841628bace1bec4f9421d041262fab0942
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-05-07 15:29:00 +0800

    Installer

---
+++ Installer/Installer.cs
@@ -0,0 +1,198 @@
+using System.Diagnostics;
+using System.Text;
+using Spectre.Console;
+
+namespace FortRise.Installer;
+
+public static class Installer
+{
+    public const string TowerFallVersion = "1.3.3.3";
+    public const string InstallerVersion = "1.0.0";
+
+    private static readonly string[] fileDependencies = {
+        "MonoMod.exe", "MonoMod.xml",
+        "MonoMod.Utils.dll", "MonoMod.Utils.xml", 
+        "MonoMod.RuntimeDetour.HookGen.exe", "MonoMod.RuntimeDetour.HookGen.xml",
+        "MonoMod.RuntimeDetour.dll", "MonoMod.RuntimeDetour.xml",
+        "Mono.Cecil.dll", "Mono.Cecil.Mdb.dll", "Mono.Cecil.Pdb.dll",
+        "TeuJson.dll"
+    };
+
+    private static readonly string modFile = "TowerFall.FortRise.mm.dll";
+
+    public static async Task Install(string path) 
+    {
+        var patchVersion = Path.Combine(path, "PatchVersion.txt");
+        bool shouldProceed = true;
+        if (File.Exists(patchVersion)) 
+        {
+            using var fs = File.OpenRead(patchVersion);
+            using var sr = new StreamReader(fs);
+            var tfVersion = sr.ReadLine()?.Split(':')[1].Trim();
+            var pVersion = sr.ReadLine()?.Split(':')[1].Trim();
+            if (pVersion == InstallerVersion) 
+            {
+                shouldProceed = false;
+            }
+        }
+        if (!shouldProceed) 
+        {
+            ThrowError("TowerFall has already been patched with FortRise.");
+            return;
+        }
+
+        AnsiConsole.MarkupLine("[underline]Moving original TowerFall into fortOrig Folder[/]");
+        var fortOrigPath = Path.Combine(path, "fortOrig");
+        if (!Directory.Exists(fortOrigPath))
+            Directory.CreateDirectory(fortOrigPath);
+        
+        if (!File.Exists(Path.Combine(fortOrigPath, "TowerFall.exe")))
+            File.Copy(Path.Combine(path, "TowerFall.exe"), Path.Combine(fortOrigPath, "TowerFall.exe"));
+
+        if (!File.Exists(Path.Combine(fortOrigPath, "TowerFall.exe")))
+        {
+            ThrowError("[underline][red]Copying failed[/][/]");           
+            return;
+        }
+        var libPath = "lib";
+        AnsiConsole.MarkupLine("[underline]Moving the mod into TowerFall directory[/]");
+
+        var fortRiseDll = Path.Combine(libPath, modFile);
+        if (!File.Exists(fortRiseDll)) 
+        {
+            ThrowError("[underline][red]TowerFall.FortRise.mm.dll mod file not found![/][/]");
+            return;
+        }
+        File.Copy(fortRiseDll, Path.Combine(path, "TowerFall.FortRise.mm.dll"), true);
+
+
+        AnsiConsole.MarkupLine("[underline]Moving all of the lib files[/]");
+        foreach (var file in fileDependencies) 
+        {
+            var filePath = Path.Combine(libPath, path);
+            var lib = Path.Combine(libPath, file);
+            if (!File.Exists(lib)) 
+            {
+                ThrowErrorContinous($"[underline][red]{lib} file not found![/][/]");
+                continue;
+            }
+
+            File.Copy(lib, Path.Combine(path, Path.GetFileName(lib)), true);
+        }
+
+        AnsiConsole.MarkupLine("[underline]Patching TowerFall[/]");
+
+        var monoModPath = Path.Combine(libPath, "MonoMod.exe");
+        if (!File.Exists(monoModPath))
+        {
+            ThrowError("No MonoMod executable found in the lib path");
+            return;
+        }
+        var process = Process.Start(monoModPath, $"\"{Path.Combine(path, "TowerFall.exe")}\"");
+        await process.WaitForExitAsync();
+        if (process.ExitCode != 0) 
+        {
+            ThrowError("[underline][red]MonoMod failed to patch the assembly[/][/]");
+            AnsiConsole.MarkupLine("[underline][gray]Note that the TowerFall might be patched from other modloader[/][/]");
+            await Task.Delay(1000);
+            return;
+        }
+
+        AnsiConsole.MarkupLine("[underline]Running HookGen[/]");
+        var hookGenPath = Path.Combine(libPath, "MonoMod.RuntimeDetour.HookGen.exe");
+        if (!File.Exists(hookGenPath))
+        {
+            ThrowError("No MonoMod.RuntimeDetour.HookGen executable found in the lib path");
+            return;
+        }
+        process = Process.Start(hookGenPath, $"\"{Path.Combine(path, "TowerFall.exe")}\"");
+        await process.WaitForExitAsync();
+        if (process.ExitCode != 0) 
+        {
+            ThrowError("[underline][red]HookGen failed to generate hooks[/][/]");
+        }
+
+        AnsiConsole.MarkupLine("[yellow]Finalizing[/]");
+
+        AnsiConsole.MarkupLine("[underline]Renaming the output[/]");
+        File.Copy(Path.Combine(path, "MONOMODDED_TowerFall.exe"), Path.Combine(path, "TowerFall.exe"), true);
+        File.Copy(Path.Combine(path, "MONOMODDED_TowerFall.pdb"), Path.Combine(path, "TowerFall.pdb"), true);
+        File.Delete(Path.Combine(path, "MONOMODDED_TowerFall.exe"));
+        File.Delete(Path.Combine(path, "MONOMODDED_TowerFall.pdb"));
+
+        AnsiConsole.MarkupLine("[underline]Writing the version file[/]");
+        var sb = new StringBuilder();
+        sb.AppendLine("TF Version: " + TowerFallVersion);
+        sb.AppendLine("Installer Version: " + InstallerVersion);
+        var text = sb.ToString();
+        await File.WriteAllTextAsync(Path.Combine(path, "PatchVersion.txt"), sb.ToString());
+
+        AnsiConsole.MarkupLine("[green]Installed![/]");
+    }
+
+    public static async Task Uninstall(string path) 
+    {
+        var patchVersion = Path.Combine(path, "PatchVersion.txt");
+        bool shouldProceed = false;
+        if (File.Exists(patchVersion)) 
+        {
+            shouldProceed = true;
+        }
+        if (!shouldProceed) 
+        {
+            ThrowError("This TowerFall has not been patched yet.");
+            return;
+        }
+        var fortOrigPath = Path.Combine(path, "fortOrig", "TowerFall.exe");
+        AnsiConsole.MarkupLine("[underline]Copying original TowerFall into TowerFall root folder[/]");
+        File.Copy(fortOrigPath, Path.Combine(path, "TowerFall.exe"), true);
+
+
+        AnsiConsole.MarkupLine("[underline]Deleting libraries from the TowerFall root folder[/]");
+        foreach (var file in fileDependencies) 
+        {
+            var lib = Path.Combine(path, file);
+            if (!File.Exists(lib)) 
+            {
+                continue;
+            }
+
+            File.Delete(lib);
+        }
+
+        AnsiConsole.MarkupLine("[underline]Deleting the mod[/]");
+
+        var fortRiseDll = Path.Combine(path, modFile);
+        if (File.Exists(fortRiseDll)) 
+        {
+            File.Delete(fortRiseDll);
+        }
+
+        AnsiConsole.MarkupLine("[underline]Deleting the hooks[/]");
+
+        var hookDll = Path.Combine(path, "MMHOOK_TowerFall.dll");
+        if (File.Exists(hookDll)) 
+        {
+            File.Delete(hookDll);
+        }
+
+        AnsiConsole.MarkupLine("[underline]Deleting the PatchVersion text file[/]");
+
+        
+        File.Delete(Path.Combine(path, "PatchVersion.txt"));
+        await Task.Delay(1000);
+
+        AnsiConsole.MarkupLine("[underline][green]Unpatched[/][/]");
+    }
+
+
+    private static void ThrowErrorContinous(string error) 
+    {
+        AnsiConsole.MarkupLine(error);
+    }
+
+    private static void ThrowError(string error) 
+    {
+        AnsiConsole.Prompt(new TextPrompt<string>(error).AllowEmpty());
+    }
+}
---
+++ Installer/Program.cs
@@ -0,0 +1,121 @@
+﻿using FortRise.Installer;
+using NativeFileDialogSharp;
+using Spectre.Console;
+
+internal class Program 
+{
+    [STAThread]
+    public async static Task Main() 
+    {
+        var panel = new Panel("FortRise Installer") {
+            Border = BoxBorder.Rounded,
+            Padding = new Padding(2, 2, 2, 2),
+            Expand = true
+        };
+
+        AnsiConsole.Write(panel);
+
+        while (true) 
+        {
+            var stateSelection = AnsiConsole.Prompt(
+                new SelectionPrompt<MenuState>()
+                    .Title("[underline]Main Menu[/]")
+                    .PageSize(10)
+                    .AddChoices(new [] { MenuState.Patch, MenuState.Unpatch, MenuState.Quit})
+            );
+
+            switch (stateSelection) 
+            {
+            case MenuState.Patch:
+                await StatePatch();
+                break;
+            case MenuState.Unpatch:
+                await StateUnpatch();
+                break;
+            case MenuState.Quit:
+                goto End;
+            }
+        }
+        End:
+        AnsiConsole.WriteLine("Goodbye!");
+    }
+
+    public static async Task StateUnpatch() 
+    {
+        AnsiConsole.MarkupLine("Select a TowerFall directory to unpatch");
+        var dialog = Dialog.FolderPicker();
+        if (dialog.IsCancelled || dialog.IsError)  
+        {
+            AnsiConsole.MarkupLine("[red]Cancelled[/]");
+            return;
+        }
+        var path = dialog.Path;
+        AnsiConsole.WriteLine("Trying to find TowerFall executable in this directory.");
+        if (!File.Exists(Path.Combine(path, "TowerFall.exe"))) 
+        {
+            AnsiConsole.MarkupLine("[underline][red]TowerFall not found! Aborting[/][/]");
+            await Task.Delay(1000);
+            return;
+        }
+
+        if (!AnsiConsole.Confirm($"""
+        Are you sure you want to unpatch this directory?
+        [yellow]{path}[/]
+        """))
+        {
+            AnsiConsole.MarkupLine("[red]Cancelled[/]");
+            return;
+        }
+        await Installer.Uninstall(path);
+    }
+
+    public static async Task StatePatch() 
+    {
+        AnsiConsole.MarkupLine("Select a TowerFall directory to patch");
+        var dialog = Dialog.FolderPicker();
+        if (dialog.IsCancelled || dialog.IsError)  
+        {
+            AnsiConsole.MarkupLine("[red]Cancelled[/]");
+            return;
+        }
+        var path = dialog.Path;
+
+        
+        AnsiConsole.WriteLine("Trying to find TowerFall executable in this directory.");
+        if (!File.Exists(Path.Combine(path, "TowerFall.exe"))) 
+        {
+            AnsiConsole.MarkupLine("[underline][red]TowerFall not found! Aborting[/][/]");
+            await Task.Delay(1000);
+            return;
+        }
+        AnsiConsole.MarkupLine("[underline][green]TowerFall found in this directory! [/][/]");
+        await Task.Delay(1000);
+
+        AnsiConsole.WriteLine("Checking if the TowerFall is from Steam");
+        if (!File.Exists(Path.Combine(path, "Steamworks.NET.dll")))
+            AnsiConsole.MarkupLine("[underline][green]TowerFall is pure[/][/]");
+        else 
+            AnsiConsole.MarkupLine("[underline][green]TowerFall is from Steam[/][/]");
+        
+        await Task.Delay(1000);
+
+
+        if (!AnsiConsole.Confirm($"""
+        Are you sure you want to patch this directory?
+        [yellow]{path}[/]
+        [gray]Make sure that this TowerFall has not been patched yet.[/]
+        """))
+        {
+            AnsiConsole.MarkupLine("[red]Cancelled[/]");
+            return;
+        }
+        await Installer.Install(path);
+    }
+}
+
+public enum MenuState 
+{
+    Patch,
+    Unpatch,
+    Quit
+}
