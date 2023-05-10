2cadc132baa45a42c2d22959e9d01baffc5fd3d8
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-05-10 17:18:46 +0800

    Custom Music API for ArcherData

---
+++ TowerFall.FortRise.mm/Patches/ArcherData.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Xml;
+using FortRise;
+using Microsoft.Xna.Framework.Audio;
+using Monocle;
+
+namespace TowerFall;
+
+public class patch_ArcherData : ArcherData
+{
+    private SoundEffectInstance instance;
+    public patch_ArcherData(XmlElement xml) : base(xml)
+    {
+    }
+
+    public void PlayCustomVictoryMusic(string archerPath) 
+    {
+        Music.Stop();
+        if (instance == null) 
+        {
+            var victorySpan = VictoryMusic.AsSpan().Slice(7);
+            var localPath = victorySpan.ToString();
+            var path = Path.Combine(archerPath, localPath);
+
+            if (!File.Exists(path)) 
+            {
+                Music.PlayImmediate("VictoryBlue");
+                Logger.Log($"Path: {path} for Archer Victory music does not exists. Falling back to Blue Victory Music");
+                return;
+            }
+            if (!SoundHelper.StoredInstance.ContainsKey(localPath)) 
+            {
+                SoundHelper.PathToSound(path, out instance);
+                SoundHelper.StoredInstance.Add(path, instance);
+            } 
+            else 
+            {
+                instance = SoundHelper.StoredInstance[localPath];
+            }
+        }
+        instance.Play();
+    }
+}
