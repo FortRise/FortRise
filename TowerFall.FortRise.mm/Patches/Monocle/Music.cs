85164c23dfb1a03cbabad98c6275ec5ee756c185
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-05-12 22:06:34 +0800

    Fixed current song is not modified properly

--- TowerFall.FortRise.mm/Patches/Monocle/Music.cs
+++ TowerFall.FortRise.mm/Patches/Monocle/Music.cs
@@ -126,6 +126,7 @@ public static class patch_Music
         if (currentSong != null && SoundHelper.StoredInstance.TryGetValue(currentSong, out var instance) 
             && instance.State == SoundState.Playing)
         {
+            currentSong = null;
             instance.Stop();
             return;
         }
