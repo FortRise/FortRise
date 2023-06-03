a868d966f89221623f6e7c5a2337284a759949a1
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-06-03 22:40:46 +0800

    Update old-built.bat

--- TowerFall.FortRise.mm/old-build.bat
+++ TowerFall.FortRise.mm/old-build.bat
@@ -1,6 +1,10 @@
 dotnet build -c Release
 cd bin/Release/net472
 copy TowerFall.FortRise.mm.dll "../../../../../../TowerFall.FortRise.mm.dll"
+copy TowerFall.FortRise.mm.pdb "../../../../../../TowerFall.FortRise.mm.pdb"
+copy TowerFall.FortRise.mm.dll "../../../../Installer/lib/TowerFall.FortRise.mm.dll"
+copy TowerFall.FortRise.mm.pdb "../../../../Installer/lib/TowerFall.FortRise.mm.pdb"
+
 copy TeuJson.dll "../../../../../../TeuJson.dll"
 cd ../../../../../../orig
 copy "TowerFall.exe" "../TowerFall.exe"
