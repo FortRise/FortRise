1fc6e72fded27f1a0af9e4835a77f164a320133a
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-06-11 14:29:22 +0800

    Remove unnecessary usings

--- TowerFall.FortRise.mm/Patches/LevelLoaderXML.cs
+++ TowerFall.FortRise.mm/Patches/LevelLoaderXML.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Xml;
 using FortRise;
-using Microsoft.Xna.Framework;
 using Monocle;
 using MonoMod;
 
