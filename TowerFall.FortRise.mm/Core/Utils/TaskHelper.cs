c3f3128055a8c5498087b084236851fc6500c1b1
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-07-07 00:23:57 +0800

    Added TaskHelper for multithreading

---
+++ TowerFall.FortRise.mm/Core/Utils/TaskHelper.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FortRise;
+
+public static class TaskHelper 
+{
+    private static ConcurrentDictionary<string, object> taskMap = new();
+    public static ICollection<string> Tasks => taskMap.Keys;
+
+    public static void Erase(string id) 
+    {
+        if (!taskMap.TryRemove(id, out _))
+            throw new Exception("Task cancellation failed!");
+    }
+
+    public static Task Run(string id, Action func) 
+    {
+        var task = taskMap.GetOrAdd(id, id => {
+            return Task.Run(() => {
+                func?.Invoke();
+                Erase(id);
+            });
+        });
+        return (Task)task;
+    }
+
+    public static bool Wait(string id) 
+    {
+        return !taskMap.ContainsKey(id);
+    }
+
+    public static bool WaitForAll() 
+    {
+        return taskMap.Count != 0;
+    }
+}
