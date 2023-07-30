9e16275835a03c55d125f85d327447815c4e273c
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-07-30 16:28:48 +0800

    Removed unused function

--- TowerFall.FortRise.mm/Patches/GameData.cs
+++ TowerFall.FortRise.mm/Patches/GameData.cs
@@ -189,15 +189,5 @@ public static class patch_GameData
         }
         return false;
     }
-
-    /// <summary>
-    /// Load Adventure towers by directory, and specify its metadata or null if it's global.
-    /// </summary>
-    /// <param name="directory">A directory path to the levels</param>
-    /// <returns>A boolean determines whether the load success or fails</returns>
-    public static bool LoadAdventureLevelsParallel(string directory) 
-    {
-        return LoadAdventureTowers(directory, null);
-    }
 }
 
