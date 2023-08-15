3965e1db19c10dac50c7d014cbee8e53dcfe6cdf
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-08-15 21:55:35 +0800

    Fixed FNA graphics device being called twice

--- TowerFall.FortRise.mm/Patches/Monocle/Screen.cs
+++ TowerFall.FortRise.mm/Patches/Monocle/Screen.cs
@@ -1,5 +1,7 @@
 using System;
+using Microsoft.Xna.Framework.Graphics;
 using Mono.Cecil;
+using Mono.Cecil.Cil;
 using MonoMod;
 using MonoMod.Cil;
 using MonoMod.Utils;
@@ -8,13 +10,47 @@ namespace Monocle
 {
     public class patch_Screen : Screen
     {
+        private Viewport viewport;
+        private float scale;
         public patch_Screen(Engine engine, int width, int height, float scale) : base(engine, width, height, scale)
         {
         }
 
         [MonoModIgnore]
+        [MonoModConstructor]
+        [PatchScreenCtor]
+        public extern void ctor(Engine engine, int width, int height, float scale);
+
+        [MonoModIgnore]
         [PatchScreenResize]
         public extern void Resize(int width, int height, float scale);
+
+
+        [MonoModReplace]
+        private void SetWindowSize(int width, int height, bool init = false)
+		{
+			this.Graphics.IsFullScreen = false;
+			this.Graphics.PreferredBackBufferWidth = width;
+			this.Graphics.PreferredBackBufferHeight = height;
+			if (!init)
+			{
+				this.Graphics.ApplyChanges();
+			}
+			this.viewport.Width = width;
+			this.viewport.Height = this.ScaledHeight;
+			this.viewport.X = 0;
+			this.viewport.Y = (height - this.ScaledHeight) / 2;
+			this.DrawRect.X = width / 2 - this.ScaledWidth / 2;
+			this.UpdatePadRects();
+			this.Matrix = Microsoft.Xna.Framework.Matrix.CreateScale(this.scale) * Microsoft.Xna.Framework.Matrix.CreateTranslation((float)this.DrawRect.X, 0f, 0f);
+		}
+
+        [MonoModIgnore]
+        [PatchScreenSetWindowSize]
+        private extern void SetWindowSize(int width, int height);
+
+        [MonoModIgnore]
+        private extern void UpdatePadRects();
     }
 }
 
@@ -23,8 +59,27 @@ namespace MonoMod
     [MonoModCustomMethodAttribute(nameof(MonoModRules.PatchScreenResize))]
     internal class PatchScreenResize : Attribute {}
 
+    [MonoModCustomMethodAttribute(nameof(MonoModRules.PatchScreenCtor))]
+    internal class PatchScreenCtor : Attribute {}
+
+    [MonoModCustomMethodAttribute(nameof(MonoModRules.PatchSetWindowSize))]
+    internal class PatchScreenSetWindowSize: Attribute {}
+
     internal static partial class MonoModRules 
     {
+        public static void PatchSetWindowSize(ILContext ctx, CustomAttribute attrib) 
+        {
+            var typeRef = ctx.Module.ImportReference(
+                typeof(System.String));
+            var obsoleteAttributeRef = ctx.Module.ImportReference(
+                typeof(System.ObsoleteAttribute)
+            .GetConstructor(new Type[1] { typeof(System.String) }));
+            var obsolete = new CustomAttribute(obsoleteAttributeRef);
+            obsolete.ConstructorArguments.Add(new CustomAttributeArgument(typeRef, "For compability sake, please use SetWindowSize(int, int, bool) with the third argument being `false`"));
+            ctx.Method.CustomAttributes.Add(obsolete);
+        }
+
+
         public static void PatchScreenResize(ILContext ctx, CustomAttribute attrib) 
         {
             var height = ctx.Method.DeclaringType.FindField("height");
@@ -34,6 +89,18 @@ namespace MonoMod
             if (cursor.TryGotoNext(instr => instr.MatchStfld("Monocle.Screen", "width"))) 
             {
                 cursor.Next.Operand = height;
+            }
+        }
+
+        public static void PatchScreenCtor(ILContext ctx, CustomAttribute attrib) 
+        {
+            var SetWindowSize = ctx.Method.DeclaringType.FindMethod("System.Void SetWindowSize(System.Int32,System.Int32,System.Boolean)");
+            var cursor = new ILCursor(ctx);
+
+            if (cursor.TryGotoNext(instr => instr.MatchCallOrCallvirt("Monocle.Screen", "SetWindowSize"))) 
+            {
+                cursor.Next.Operand = SetWindowSize;
+                cursor.Emit(OpCodes.Ldc_I4_1);
             }
         }
     }
