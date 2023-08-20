39d7f8aefadf652af1520b8ab5e19b49693235de
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-08-20 18:26:39 +0800

    Level IDs for vanilla towers

--- TowerFall.FortRise.mm/Patches/GameData.cs
+++ TowerFall.FortRise.mm/Patches/GameData.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using FortRise;
 using FortRise.Adventure;
 
@@ -16,9 +17,37 @@ public static class patch_GameData
         RiseCore.GameData.Load();
 
         // Assign its LevelID
+        foreach (var questTowers in GameData.QuestLevels) 
+        {
+            var name = questTowers.Theme.Name;
+            var correctName = CultureInfo.InvariantCulture.TextInfo.ToTitleCase(name.ToLower());
+            questTowers.SetLevelID(correctName);
+        }
+
+        foreach (var versusTowers in GameData.VersusTowers) 
+        {
+            var name = versusTowers.Theme.Name;
+            var correctName = CultureInfo.InvariantCulture.TextInfo.ToTitleCase(name.ToLower());
+            versusTowers.SetLevelID(correctName);
+        }
+
         foreach (var darkWorldTowers in GameData.DarkWorldTowers) 
         {
-            darkWorldTowers.SetLevelID("TowerFall");
+            var name = darkWorldTowers.Theme.Name;
+            var correctName = CultureInfo.InvariantCulture.TextInfo.ToTitleCase(name.ToLower());
+            darkWorldTowers.SetLevelID(correctName);
+        }
+
+        foreach (var trialTowers in GameData.TrialsLevels) 
+        {
+            var name = trialTowers.Theme.Name;
+            var id = trialTowers.ID.Y switch {
+                0 => "I",
+                1 => "II",
+                _ => "III"
+            };
+            var correctName = CultureInfo.InvariantCulture.TextInfo.ToTitleCase(name.ToLower()) + " " + id;
+            trialTowers.SetLevelID(correctName);
         }
         TFGame.WriteLineToLoadLog("Loading Adventure World Tower Data...");
 
