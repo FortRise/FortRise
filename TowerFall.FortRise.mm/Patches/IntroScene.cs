edb974fcb859dda5390983ac651293df2fda64ee
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-09-14 15:06:06 +0800

    Fixed missing MMG.wav on 1.3.3.1

--- TowerFall.FortRise.mm/Patches/IntroScene.cs
+++ TowerFall.FortRise.mm/Patches/IntroScene.cs
@@ -1,15 +1,52 @@
+using System;
+using System.Collections;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
 using Monocle;
 using MonoMod;
+using MonoMod.Cil;
+using MonoMod.Utils;
 
-namespace TowerFall;
-
-public class patch_IntroScene : IntroScene 
+namespace TowerFall 
 {
-    [MonoModReplace]
-    private Subtexture GetLetterSub(int letterIndex) 
+    public class patch_IntroScene : IntroScene 
     {
-        if (patch_TFGame.FortRiseMenuAtlas.Contains("mmg/" + letterIndex))
-            return patch_TFGame.FortRiseMenuAtlas["mmg/" + letterIndex];
-        return TFGame.MenuAtlas["mmg/" + letterIndex];
+        [MonoModReplace]
+        private Subtexture GetLetterSub(int letterIndex) 
+        {
+            if (patch_TFGame.FortRiseMenuAtlas.Contains("mmg/" + letterIndex))
+                return patch_TFGame.FortRiseMenuAtlas["mmg/" + letterIndex];
+            return TFGame.MenuAtlas["mmg/" + letterIndex];
+        }
+
+        [MonoModIgnore]
+        [PatchIntroSceneSequence]
+        private extern IEnumerator Sequence();
+    }
+}
+
+namespace MonoMod 
+{
+    [MonoModCustomMethodAttribute(nameof(MonoModRules.PatchIntroSceneSequence))]
+    internal class PatchIntroSceneSequence : Attribute {}
+
+    internal static partial class MonoModRules 
+    {
+
+        public static void PatchIntroSceneSequence(MethodDefinition method, CustomAttribute attrib) 
+        {
+            var complete = method.GetEnumeratorMoveNext();
+
+            new ILContext(complete).Invoke(ctx => {
+                var ORIGINAL_LOAD_PREFIX = ctx.Module.GetType("Monocle.Audio")
+                    .FindField("ORIGINAL_LOAD_PREFIX");
+                var cursor = new ILCursor(ctx);
+
+                if (cursor.TryGotoNext(MoveType.Before, instr => instr.MatchLdsfld("Monocle.Audio", "LOAD_PREFIX"))) 
+                {
+                    cursor.Next.Operand = ORIGINAL_LOAD_PREFIX;
+                }
+            });
+        }
     }
 }
