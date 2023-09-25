8019f8593665f9d1b4835c32adc6b5e6876235fa
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-09-25 17:09:49 +0800

    Removed BepInEx build script

--- TowerFall.FortRise.mm/build.bat
+++ TowerFall.FortRise.mm/build.bat
@@ -1,7 +1,17 @@
 dotnet build -c Release
 cd bin/Release/net472
-copy TowerFall.FortRise.mm.dll "../../../../../BepInEx/monomod/TowerFall.FortRise.mm.dll"
-copy TeuJson.dll "../../../../../BepInEx/monomod/TeuJson.dll"
+copy TowerFall.FortRise.mm.dll "../../../../../../Installer-Framework/TowerFall.FortRise.mm.dll"
+copy TowerFall.FortRise.mm.pdb "../../../../../../Installer-Framework/TowerFall.FortRise.mm.pdb"
+copy TowerFall.FortRise.mm.xml "../../../../../../Installer-Framework/TowerFall.FortRise.mm.xml"
+copy TowerFall.FortRise.mm.dll "../../../../Installer/lib/TowerFall.FortRise.mm.dll"
+copy TowerFall.FortRise.mm.pdb "../../../../Installer/lib/TowerFall.FortRise.mm.pdb"
+copy TowerFall.FortRise.mm.xml "../../../../Installer/lib/TowerFall.FortRise.mm.xml"
 
+
+cd ../../../../../../orig
+copy "TowerFall.exe" "../TowerFall.exe"
+cd ../Installer-Framework
+Installer.NoAnsi.exe --patch "../"
+cd ../MonoMod/TowerFallMM
 
 pause
