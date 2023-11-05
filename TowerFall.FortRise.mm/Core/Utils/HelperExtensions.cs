97d2a7856ec95a62567c1eb3a160e15b5771ff71
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-11-05 20:45:38 +0800

    Extensions for DynamicData

--- TowerFall.FortRise.mm/Core/Utils/HelperExtensions.cs
+++ TowerFall.FortRise.mm/Core/Utils/HelperExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using MonoMod.Utils;
 using Ionic.Zip;
 
 namespace FortRise;
@@ -15,5 +16,20 @@ public static class HelperExtensions
         entry.Extract(memStream);
         memStream.Seek(0, SeekOrigin.Begin);
         return memStream;
+    }
+
+    public static DynamicData Dynamic(this object obj) 
+    {
+        return DynamicData.For(obj);
+    }
+
+    public static T GetData<T>(this object obj, string name) 
+    {
+        return DynamicData.For(obj).Get<T>(name);
+    }
+
+    public static void SetData(this object obj, string name, object value) 
+    {
+        DynamicData.For(obj).Set(name, value);
     }
 }
