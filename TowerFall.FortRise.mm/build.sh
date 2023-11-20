9bbb59cc8c8be2faf307ea74d449b4b1ada6fa45
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-11-20 14:30:03 +0800

    use shell script for building

---
+++ TowerFall.FortRise.mm/build.sh
@@ -0,0 +1,44 @@
+#!usr/bin/bash
+
+FileCopy() {
+	File="$1"
+	Destination="../../../../../../Installer-Framework/$1"
+	echo "Copying $File to $Destination"
+	cp $File $Destination
+
+	Destination="../../../../Installer/lib-framework/$1"
+	echo "Copying $File to $Destination"
+	cp $File $Destination
+}
+
+dotnet build -c Release
+
+if [ $? -ne 0 ]; then
+	# dotnet fails building the mod rules
+	echo
+	echo -e "Failed to build the mod rules!"
+	echo
+	exit 1
+fi
+# Proceed if succeed
+
+cd "bin/Release/net472"
+
+FileCopy "TowerFall.FortRise.mm.dll"
+FileCopy "TowerFall.FortRise.mm.pdb"
+FileCopy "TowerFall.FortRise.mm.xml"
+
+echo "Proceeding to backup the original TowerFall"
+cd "../../../../../../orig"
+cp "TowerFall.exe" "../TowerFall.exe"
+cd "../Installer-Framework"
+
+echo "Patching TowerFall with installer"
+./Installer.NoAnsi.exe --patch "../"
+cd "../MonoMod/TowerFallMM"
+
+echo
+echo "Success!"
+echo
+
+read -p "Press enter to continue!"
