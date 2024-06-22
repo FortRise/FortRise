4ae162fbfb51434f4249e93d562352d304c936ee
Terria-K <aelbrian028@gmail.com>
2024-06-22 13:57:43 +0800

    Fixed stream closing prematurely when being used by other stream.

--- TowerFall.FortRise.mm/Core/Utils/ModIO.cs
+++ TowerFall.FortRise.mm/Core/Utils/ModIO.cs
@@ -7,21 +7,26 @@ using Monocle;
 // I am using namespace now for transition with FortRise 5
 namespace FortRise.IO;
 
-public static class ModIO 
+/// <summary>
+/// Utility class that can access to mods' content or outside of the mods' content in a portable way.
+/// Mod's content path generally starts with the prefix of 'mod:{mod_metadata_name}/' where 'mod_metadata_name' is
+/// the name of the Mod in its metadata name.
+/// </summary>
+public static class ModIO
 {
-    public static bool IsFolder(RiseCore.Resource resource) 
+    public static bool IsFolder(RiseCore.Resource resource)
     {
         return resource.ResourceType == typeof(RiseCore.ResourceTypeFolder);
     }
 
-    public static bool IsFile(RiseCore.Resource resource) 
+    public static bool IsFile(RiseCore.Resource resource)
     {
         return resource.ResourceType != typeof(RiseCore.ResourceTypeFolder);
     }
 
-    public static bool IsFolder(string path) 
+    public static bool IsFolder(string path)
     {
-        if (RiseCore.ResourceTree.TryGetValue(path, out var res)) 
+        if (RiseCore.ResourceTree.TryGetValue(path, out var res))
         {
             return IsFolder(res);
         }
@@ -29,9 +34,9 @@ public static class ModIO
         return attr == FileAttributes.Directory;
     }
 
-    public static bool IsFile(string path) 
+    public static bool IsFile(string path)
     {
-        if (RiseCore.ResourceTree.TryGetValue(path, out var res)) 
+        if (RiseCore.ResourceTree.TryGetValue(path, out var res))
         {
             return IsFile(res);
         }
@@ -39,57 +44,57 @@ public static class ModIO
         return attr != FileAttributes.Directory;
     }
 
-    public static bool IsDirectoryOrFileExists(string path) 
+    public static bool IsDirectoryOrFileExists(string path)
     {
         return RiseCore.ResourceTree.IsExist(path) || Directory.Exists(path) || File.Exists(path);
     }
 
-    public static bool IsFileExists(string path) 
+    public static bool IsFileExists(string path)
     {
         return RiseCore.ResourceTree.IsExist(path) || File.Exists(path);
     }
 
-    public static bool IsDirectoryExists(string path) 
+    public static bool IsDirectoryExists(string path)
     {
         return RiseCore.ResourceTree.IsExist(path) || Directory.Exists(path);
     }
 
-    public static XmlDocument LoadXml(RiseCore.Resource resource) 
+    public static XmlDocument LoadXml(RiseCore.Resource resource)
     {
         var text = ModIO.OpenRead(resource);
         return patch_Calc.LoadXML(text);
     }
 
-    public static XmlDocument LoadXml(string path) 
+    public static XmlDocument LoadXml(string path)
     {
-        if (RiseCore.ResourceTree.TryGetValue(path, out var resource)) 
+        if (RiseCore.ResourceTree.TryGetValue(path, out var resource))
         {
             return ModIO.LoadXml(resource);
         }
         return Calc.LoadXML(path);
     }
 
-    public static string[] GetFiles(string path) 
+    public static string[] GetFiles(string path)
     {
-        if (RiseCore.ResourceTree.TryGetValue(path, out var res)) 
+        if (RiseCore.ResourceTree.TryGetValue(path, out var res))
         {
             return GetFiles(res);
         }
         return Directory.GetFiles(path);
     }
 
-    public static string[] GetFiles(RiseCore.Resource res) 
+    public static string[] GetFiles(RiseCore.Resource res)
     {
         List<RiseCore.Resource> childs = new List<RiseCore.Resource>();
-        foreach (var r in res.Childrens) 
+        foreach (var r in res.Childrens)
         {
-            if (r.ResourceType == typeof(RiseCore.ResourceTypeFile)) 
+            if (r.ResourceType == typeof(RiseCore.ResourceTypeFile))
             {
                 childs.Add(r);
             }
         }
         string[] files = new string[childs.Count];
-        for (int i = 0; i < childs.Count; i++) 
+        for (int i = 0; i < childs.Count; i++)
         {
             var child = childs[i];
             files[i] = child.RootPath;
@@ -98,27 +103,27 @@ public static class ModIO
         return files;
     }
 
-    public static string[] GetDirectories(string path) 
+    public static string[] GetDirectories(string path)
     {
-        if (RiseCore.ResourceTree.TryGetValue(path, out var res)) 
+        if (RiseCore.ResourceTree.TryGetValue(path, out var res))
         {
             return GetDirectories(res);
         }
         return Directory.GetDirectories(path);
     }
 
-    public static string[] GetDirectories(RiseCore.Resource res) 
+    public static string[] GetDirectories(RiseCore.Resource res)
     {
         List<RiseCore.Resource> childs = new List<RiseCore.Resource>();
-        foreach (var r in res.Childrens) 
+        foreach (var r in res.Childrens)
         {
-            if (r.ResourceType == typeof(RiseCore.ResourceTypeFolder)) 
+            if (r.ResourceType == typeof(RiseCore.ResourceTypeFolder))
             {
                 childs.Add(r);
             }
         }
         string[] directories = new string[childs.Count];
-        for (int i = 0; i < childs.Count; i++) 
+        for (int i = 0; i < childs.Count; i++)
         {
             var child = childs[i];
             directories[i] = child.RootPath;
@@ -127,12 +132,12 @@ public static class ModIO
         return directories;
     }
 
-    public static Stream OpenRead(RiseCore.Resource resource) 
+    public static Stream OpenRead(RiseCore.Resource resource)
     {
         return resource.Stream;
     }
 
-    public static Stream OpenRead(string path) 
+    public static Stream OpenRead(string path)
     {
         if (RiseCore.ResourceTree.TryGetValue(path, out var res))
         {
@@ -142,7 +147,7 @@ public static class ModIO
         return File.OpenRead(path);
     }
 
-    public static Stream Open(string path, FileMode mode) 
+    public static Stream Open(string path, FileMode mode)
     {
         if (RiseCore.ResourceTree.TryGetValue(path, out var res))
         {
@@ -153,25 +158,23 @@ public static class ModIO
         return File.Open(path, mode);
     }
 
-    public static StreamReader OpenText(RiseCore.Resource resource) 
+    public static StreamReader OpenText(RiseCore.Resource resource)
     {
-        using var fs = resource.Stream;
+        Stream fs = resource.Stream;
         return new StreamReader(fs);
     }
 
-    public static StreamReader OpenText(string path) 
+    public static StreamReader OpenText(string path)
     {
         if (RiseCore.ResourceTree.TryGetValue(path, out var res))
         {
-            using var rfs = res.Stream;
-            StreamReader reader = new StreamReader(rfs);
-            return reader;
+            return OpenText(res);
         }
 
         return File.OpenText(path);
     }
 
-    public static void LoopAllModsContent(Action<FortContent> contentCallback) 
+    public static void LoopAllModsContent(Action<FortContent> contentCallback)
     {
         RiseCore.ResourceTree.LoopThroughModsContent(contentCallback);
     }
