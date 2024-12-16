a8ab63a92ba9111038533784e6bfd3a29ac23fee
Terria-K <Terria-K>
2024-12-17 01:08:37 +0800

    Use modified DotNetZip to fix vulnerability (main)

--- Installer/Installer.cs
+++ Installer/Installer.cs
@@ -183,7 +183,7 @@ public class Installer : MarshalByRefObject
         Underline("Writing the version file");
 
         var sb = new StringBuilder();
-        sb.AppendLine("Installer Version: " + "4.7.6");
+        sb.AppendLine("Installer Version: " + "4.7.7");
 
         var text = sb.ToString();
 
