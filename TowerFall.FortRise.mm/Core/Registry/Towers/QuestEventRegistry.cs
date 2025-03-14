c591cce2dd419d490fd0605bcf0f753ebfc832de
Terria-K <teuria@proton.me>
2025-03-14 13:57:32 +0800

    Add documentation to QuestEventRegistry

--- TowerFall.FortRise.mm/Core/Registry/Towers/QuestEventRegistry.cs
+++ TowerFall.FortRise.mm/Core/Registry/Towers/QuestEventRegistry.cs
@@ -5,12 +5,22 @@ using TowerFall;
 
 namespace FortRise;
 
-
+/// <summary>
+/// A quest event delegate used for appearing and disappearing an event.
+/// </summary>
+/// <param name="level">An in-game <see cref="TowerFall.Level"/> scene</param>
 public delegate void QuestEventAction(Level level);
+
+/// <summary>
+/// A registry to register a quest event for quest levels.
+/// </summary>
 public static class QuestEventRegistry 
 {
     private static Dictionary<string, Event> events = new Dictionary<string, Event>();
 
+    /// <summary>
+    /// A read-only events map.
+    /// </summary>
     public static IReadOnlyDictionary<string, Event> Events => events;
 
     internal static void LoadAllBuiltinEvents() 
@@ -28,11 +38,22 @@ public static class QuestEventRegistry
         (_) => Engine.Instance.Scene.Layers[0].GetFirst<BottomMiasma>()?.Dissipate());
     }
 
+    /// <summary>
+    /// Use this function inside of a <see cref="FortRise.FortModule.Initialize"/> to add a quest event to the registry.
+    /// </summary>
+    /// <param name="eventName">A name of the event</param>
+    /// <param name="appear">An appear callback used to make the event appear</param>
+    /// <param name="dieOut">A dieOut callback used to make the event disappear</param>
     public static void Register(string eventName, QuestEventAction appear, QuestEventAction dieOut)
     {
         events[eventName] = new Event(appear, dieOut);
     }
 
+    /// <summary>
+    /// A struct containing the callbacks to the quest events.
+    /// </summary>
+    /// <param name="appear">An appear callback used to make the event appear</param>
+    /// <param name="dieOut">A dieOut callback used to make the event disappear</param>
     public struct Event(QuestEventAction appear, QuestEventAction dieOut)
     {
         public QuestEventAction Appear = appear;
