df164798a798ec9fd3ec68ca06b29c096543c4fc
Terria-K <teuria@proton.me>
2025-07-08 20:30:42 +0800

    Locked RollcallElement when there is not enough archers

--- TowerFall.FortRise.mm/Patches/RollcallElement.cs
+++ TowerFall.FortRise.mm/Patches/RollcallElement.cs
@@ -1,3 +1,4 @@
+using System;
 using FortRise;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
@@ -14,8 +15,124 @@ public class patch_RollcallElement : RollcallElement
     private ArcherData.ArcherTypes archerType;
     private int playerIndex;
 
-    public patch_RollcallElement(int playerIndex) : base(playerIndex)
+    private Wiggler arrowWiggle;
+    private Wiggler altWiggle;
+    private StateMachine state;
+    private float altEase;
+    private Vector2 tweenTo;
+    private Vector2 tweenFrom;
+    private PlayerInput input;
+    private SineWave arrowSine;
+    private SineWave darkWorldAlphaSine;
+    private bool isLocked;
+
+    public patch_RollcallElement(int playerIndex) : base(playerIndex) { }
+
+    [MonoModLinkTo("TowerFall.MenuItem", "System.Void .ctor(Microsoft.Xna.Framework.Vector2)")]
+    public void base_ctor(Vector2 position) {}
+
+    [MonoModReplace]
+    [MonoModConstructor]
+    public void ctor(int playerIndex)
     {
+        base_ctor(GetPosition(playerIndex));
+
+        bool shouldLock = playerIndex >= ArcherData.Archers.Length - FortRiseModule.Settings.BlacklistedArcher.Count;
+        isLocked = shouldLock;
+
+        this.playerIndex = playerIndex;
+        tweenTo = Position;
+        tweenFrom = GetTweenSource(playerIndex);
+
+        if (!shouldLock)
+        {
+            input = TFGame.PlayerInputs[playerIndex];
+
+            if (!TFGame.Players[playerIndex])
+            {
+                while (TFGame.CharacterTaken(CharacterIndex) ||
+                    !SaveData.Instance.Unlocks.GetArcherUnlocked(CharacterIndex) ||
+                    IsArcherBlacklisted(CharacterIndex))
+                {
+                    CharacterIndex += 1;
+                }
+            }
+        }
+
+        portrait = new ArcherPortrait(Vector2.Zero, TFGame.Characters[playerIndex], archerType, true);
+        Add(portrait);
+        portrait.Visible = !shouldLock;
+
+        if (!TFGame.Players[playerIndex] && TFGame.AltSelect[playerIndex] == ArcherData.ArcherTypes.Secret)
+        {
+            TFGame.AltSelect[playerIndex] = ArcherData.ArcherTypes.Normal;
+        }
+
+        archerType = TFGame.AltSelect[playerIndex];
+
+        rightArrow = new Image(TFGame.MenuAtlas["portraits/arrow"])
+        {
+            Visible = false
+        };
+
+        rightArrow.CenterOrigin();
+
+        Add(rightArrow);
+        leftArrow = new Image(TFGame.MenuAtlas["portraits/arrow"])
+        {
+            FlipX = true,
+            Visible = false
+        };
+
+        leftArrow.CenterOrigin();
+
+        Add(leftArrow);
+
+        leftArrow.Y = rightArrow.Y = 60f;
+        arrowWiggle = Wiggler.Create(20, 6f, Ease.CubeIn);
+
+        Add(arrowWiggle);
+        Add(arrowSine = new SineWave(120));
+
+        altWiggle = Wiggler.Create(30, 4f);
+        Add(altWiggle);
+
+        state = new StateMachine(2);
+        state.SetCallbacks(0, new Func<int>(NotJoinedUpdate), new Action(EnterNotJoined), new Action(LeaveNotJoined));
+        state.SetCallbacks(1, new Func<int>(JoinedUpdate), new Action(EnterJoined), new Action(LeaveJoined));
+        state.SetCallbacks(2, null);
+
+        Add(state);
+
+        HandleControlIcons();
+        HandleState();
+
+        if (TFGame.Players[playerIndex])
+        {
+            portrait.StartJoined();
+        }
+
+        if (archerType == ArcherData.ArcherTypes.Alt)
+        {
+            altEase = 1f;
+        }
+
+        GamePad.SetLightBarEXT((PlayerIndex)playerIndex, portrait.ArcherData.LightbarColor);
+        rightArrow.Color = leftArrow.Color = Color.Lerp(ArcherData.Archers[playerIndex].ColorB, Color.White, 0.5f);
+
+        Add(darkWorldAlphaSine = new SineWave(120));
+    }
+
+    [MonoModReplace]
+    private void EnforceCharacterLock(int takenIndex)
+    {
+        if (state.State != 1 && TFGame.CharacterTaken(CharacterIndex) && !isLocked)
+        {
+            if (CharacterIndex == takenIndex)
+            {
+                ChangeSelectionRight();
+            }
+        }
     }
 
     [MonoModReplace]
@@ -27,7 +144,7 @@ public class patch_RollcallElement : RollcallElement
 
         while (TFGame.CharacterTaken(CharacterIndex) ||
             !SaveData.Instance.Unlocks.GetArcherUnlocked(CharacterIndex) ||
-            IsArcherBlacklisted())
+            IsArcherBlacklisted(CharacterIndex))
         {
             num = CharacterIndex;
             CharacterIndex = num - 1;
@@ -49,7 +166,7 @@ public class patch_RollcallElement : RollcallElement
         CharacterIndex = num + 1;
         while (TFGame.CharacterTaken(CharacterIndex) ||
             !SaveData.Instance.Unlocks.GetArcherUnlocked(CharacterIndex) ||
-            IsArcherBlacklisted())
+            IsArcherBlacklisted(CharacterIndex))
         {
             num = CharacterIndex;
             CharacterIndex = num + 1;
@@ -62,16 +179,41 @@ public class patch_RollcallElement : RollcallElement
         }
     }
 
-    private bool IsArcherBlacklisted()
+    private bool IsArcherBlacklisted(int charIndex)
     {
-        var archer = ArcherRegistry.GetArcherEntry(CharacterIndex);
+        var archer = ArcherRegistry.GetArcherEntry(charIndex);
 
         if (archer is null)
         {
-            var data = ArcherData.Archers[CharacterIndex];
+            var data = ArcherData.Archers[charIndex];
             return FortRiseModule.Settings.BlacklistedArcher.Contains(data.Name0 + data.Name1);
         }
 
         return FortRiseModule.Settings.BlacklistedArcher.Contains(archer.Name);
     }
+
+
+    [MonoModIgnore]
+    private extern void HandleControlIcons();
+
+    [MonoModIgnore]
+    private extern void HandleState();
+
+    [MonoModIgnore]
+    private extern int NotJoinedUpdate();
+
+    [MonoModIgnore]
+    private extern void EnterNotJoined();
+
+    [MonoModIgnore]
+    private extern void LeaveNotJoined();
+
+    [MonoModIgnore]
+    private extern int JoinedUpdate();
+
+    [MonoModIgnore]
+    private extern void EnterJoined();
+
+    [MonoModIgnore]
+    private extern void LeaveJoined();
 }
