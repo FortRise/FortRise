e73829476925c7c7da8ae4202d90286606ba841e
Terria-K <teuria@proton.me>
2025-07-25 19:15:00 +0800

    Fixed memory leak when entering to any workshop levels

--- TowerFall.FortRise.mm/Patches/CustomLevelSystem.cs
+++ TowerFall.FortRise.mm/Patches/CustomLevelSystem.cs
@@ -1,6 +1,7 @@
 using System;
 using FortRise;
 using MonoMod;
+using SDL3;
 
 namespace TowerFall;
 
@@ -9,20 +10,67 @@ namespace TowerFall;
 public class patch_CustomLevelSystem : CustomLevelSystem
 {
     private int[] treasureMask;
+
+    public Steamworks.PublishedFileId_t WorkshopID
+    {
+        [MonoModIgnore]
+        get => default;
+        [MonoModIgnore]
+        private set { }
+    }
+
+    public bool WorkshopSubscribed
+    {
+        [MonoModIgnore]
+        get => false;
+        [MonoModIgnore]
+        private set { }
+    }
+
+    public Steamworks.EWorkshopVote WorkshopRating
+    {
+        [MonoModIgnore]
+        get => default;
+        [MonoModIgnore]
+        private set { }
+    }
+
+    private Steamworks.CallResult<Steamworks.RemoteStorageUserVoteDetails_t> ratingCallback;
+
     public patch_CustomLevelSystem(string file) : base(file)
     {
     }
     public extern void orig_ctor(string file);
 
     [MonoModConstructor]
-    public void ctor(string file) 
+    public void ctor(string file)
     {
         orig_ctor(file);
         // Resize so we don't get any error
-        Array.Resize<int>(ref treasureMask, treasureMask.Length + PickupsRegistry.GetAllPickups().Count + 1);
+        Array.Resize(ref treasureMask, treasureMask.Length + PickupsRegistry.GetAllPickups().Count + 1);
     }
 
-    [PatchSDL2ToSDL3]
+    [MonoModReplace]
+    public void StartWorkshopLookup(Steamworks.PublishedFileId_t fileID)
+    {
+        WorkshopID = fileID;
+        if (fileID.m_PublishedFileId != 0UL)
+        {
+            uint itemState = Steamworks.SteamUGC.GetItemState(fileID);
+            WorkshopSubscribed = (itemState & 1U) != 0U;
+            if (SDL.SDL_GetPlatform().Equals("Mac OS X"))
+            {
+                WorkshopRating = Steamworks.EWorkshopVote.k_EWorkshopVoteUnvoted;
+            }
+            else
+            {
+                ratingCallback = Steamworks.CallResult<Steamworks.RemoteStorageUserVoteDetails_t>.Create(null);
+                Steamworks.SteamAPICall_t userPublishedItemVoteDetails = Steamworks.SteamRemoteStorage.GetUserPublishedItemVoteDetails(WorkshopID);
+                ratingCallback.Set(userPublishedItemVoteDetails, new Steamworks.CallResult<Steamworks.RemoteStorageUserVoteDetails_t>.APIDispatchDelegate(OnRatingCheck));
+            }
+        }
+    }
+
     [MonoModIgnore]
-    public extern void StartWorkshopLookup(Steamworks.PublishedFileId_t fileID);
+    private extern void OnRatingCheck(Steamworks.RemoteStorageUserVoteDetails_t result, bool ioError);
 }
